f3649646511fdb1f2038b981efc5aee52e325199
Henry Le <140691753+hen43@users.noreply.github.com>
2024-11-12 19:02:25 +0000

    skibidi

--- Main.java
+++ Main.java
@@ -4,9 +4,15 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // You can test your method here
-    // System.out.println(isPrime(1));
-    // System.out.println(isPrime(17));
+    String word = "Peter Piper picked a pack of pickle peppers.";
+    int count = 0;
+    String{} vowels = new String{"a", "e", "i", "o", "u"};
+
+    for(i = 0; i < word.length()-1; i++){
+      if (word.substring(i, i+1) == p){
+        if vowels.word.substring(i+1, i+2)
+      }
+    }
   }
 
   public static boolean isPrime(int N)
